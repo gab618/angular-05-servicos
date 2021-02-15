2640e929e7bd8df71605d655896080afc3bbf35d
Gabriel <knuckles4x@gmail.com>
2021-02-15 17:45:53 -0300

    broadcast de eventos com event emitter

--- src/app/cursos/cursos.service.ts
+++ src/app/cursos/cursos.service.ts
@@ -1,7 +1,10 @@
-import { Injectable } from '@angular/core';
+import { EventEmitter, Injectable } from '@angular/core';
 
 @Injectable()
 export class CursosService {
+  emitirCursoCriado = new EventEmitter<string>();
+  static criouNovoCurso = new EventEmitter<string>();
+
   private cursos: string[] = ['Angular', 'Java', 'node'];
   getCursos() {
     return this.cursos;
@@ -9,5 +12,7 @@ export class CursosService {
 
   addCurso(curso: string) {
     this.cursos.push(curso);
+    this.emitirCursoCriado.emit(curso);
+    CursosService.criouNovoCurso.emit(curso);
   }
 }

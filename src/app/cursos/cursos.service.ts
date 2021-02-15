69f77b9f2330d79a5cf5206ed4aebcc95a206a83
Gabriel <knuckles4x@gmail.com>
2021-02-15 13:25:56 -0300

    singleton e varias instancias

--- src/app/cursos/cursos.service.ts
+++ src/app/cursos/cursos.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class CursosService {
+  private cursos: string[] = ['Angular', 'Java', 'node'];
   getCursos() {
-    return ['Angular', 'Java', 'node'];
+    return this.cursos;
+  }
+
+  addCurso(curso: string) {
+    this.cursos.push(curso);
   }
 }
